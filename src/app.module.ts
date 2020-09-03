bd5c65da37440f7f4f692e58f4016e25965c3346
jason <jasoa125@gmail.com>
2020-09-03 17:49:19 +0800

    add .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { MessageModule } from './message/message.module';
 import { UserModule } from './user/user.module';
@@ -9,15 +10,21 @@ import { User } from './user/model/user.model';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root1234',
-      database: 'test',
-      entities: [Message, User],
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        database: configService.get<string>('DB_NAME'),
+        username: configService.get<string>('DB_ROOT_USER'),
+        password: configService.get<string>('DB_ROOT_PASS'),
+        entities: [Message, User],
+        synchronize: true
+      }),
+      inject: [ConfigService]
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
     GraphQLModule.forRoot({
       // typePaths: ['./**/*.graphql'],
