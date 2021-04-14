47100f8e996e2b2e0b1fe770bef34f6beaa2432c
jason <jasoa125@gmail.com>
2021-04-14 11:23:02 +0800

    FEAT: add layout

---
+++ web/src/layout/AppBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app class="primary lighten-1">
+    <v-app-bar-nav-icon
+      @click="$emit('toggle-drawer')"
+      style="color: white"
+    ></v-app-bar-nav-icon>
+    <v-toolbar-title style="color: white">{{
+      $route.meta.title
+    }}</v-toolbar-title>
+    <v-spacer />
+    <v-btn icon style="color: white">
+      <v-icon>{{ icons.mdiAccount }}</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+<script>
+import { mdiAccount } from "@mdi/js"
+
+export default {
+  data() {
+    return {
+      icons: {
+        mdiAccount
+      }
+    }
+  }
+}
+</script>
---
+++ web/src/layout/AppDrawer.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-navigation-drawer v-model="myDrawer" fixed temporary>
+    <v-list>
+      <template v-for="(item, i) in 10">
+        <v-list-item
+          color="primary"
+          :key="i"
+          :to="item.link === '#' ? null : item.link"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ icons.mdiImageFilterNone }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title link>
+              {{ item }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+import { mdiCircleSmall, mdiImageFilterNone } from "@mdi/js"
+export default {
+  props: ["drawer"],
+  data() {
+    return {
+      myDrawer: null,
+      icons: {
+        mdiCircleSmall,
+        mdiImageFilterNone,
+      },
+    }
+  },
+  watch: {
+    drawer(data) {
+      this.myDrawer = data
+    },
+  },
+}
+</script>
